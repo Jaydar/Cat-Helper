a986bf5ebadaa575a919e8c6a5d54823711d98a9
Jaydar <Jaydar@Outlook.com>
2019-06-20 18:49:26 +0800

    up

--- core/Page/Color/ColorPage.tsx
+++ core/Page/Color/ColorPage.tsx
@@ -1,63 +1,139 @@
 import React from 'react';
-import { TextField, InputLabel } from "@material-ui/core";
+import { TextField,Fab,Container } from "@material-ui/core";
+import {FileCopy as FileCopyIcon} from '@material-ui/icons/';
 import "./ColorPage.scss"
 
+interface Mode {
+    mode:ColorMode,
+    topName:string,
+    btmName:string,
+    reg:RegExp,
+}
 
-enum ColorMode{ RGB="RGB",RGBA="RGBA",HEX="HEX",HEXA="HEXA",UKN="UKN" }
-
-export default class HomePage extends React.Component {
+enum ColorMode{ RGB="RGB",RGBA="RGBA",HEX="HEX",HEXA="HEXA",HEXS="HEXS",UKN="UKN" }
+  
+export default class ColorPage extends React.Component {
     
-    state = {
+    private mode:Mode[] = [
+        {
+            mode:ColorMode.RGB,
+            topName:"RGB",
+            btmName:"HEX",
+            reg:/^rgb\((25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9]),(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9]),(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9])\)$/im,
+        },
+        {
+            mode:ColorMode.RGBA,
+            topName:"RGBA",
+            btmName:"HEXA",
+            reg:/^rgba\((25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9]),(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9]),(25[0-5]|2[0-4][0-9]|[0-1]?[0-9]?[0-9]),(0(\.\d{1,2})?|1)\)$/im,
+        },
+        {
+            mode:ColorMode.HEXA,
+            topName:"HEXA",
+            btmName:"RGBA",
+            reg:/^#([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})$/,
+        },
+        {
+            mode:ColorMode.HEX,
+            topName:"HEX",
+            btmName:"RGB",
+            reg:/^#([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})([0-9A-Fa-f]{2})$/,
+        },
+        {
+            mode:ColorMode.HEXS,
+            topName:"HEX",
+            btmName:"RGB",
+            reg:/^#([0-9A-Fa-f])([0-9A-Fa-f])([0-9A-Fa-f])$/
+        }
+    ];
+
+    
+    private inputElement  = React.createRef<HTMLInputElement>();
+    private topInputElement = React.createRef<HTMLInputElement>();
+    private btmInputElement = React.createRef<HTMLInputElement>();
+
+
+    public state = {
         topInputLabelName:ColorMode.HEX,
-        bottomInputLabelName:ColorMode.RGB
+        btmInputLabelName:ColorMode.RGB,
+        topInputValue:"",
+        btmInputValue:""
+    }
+
+    public componentDidMount(){
+
+        let input = this.inputElement.current as HTMLInputElement;
+        input.focus();
+        document.execCommand("paste");
+        input.value = "#FFFFFFDB"
+        this.setValue(input.value.replace(/\s+/g, ""));
+    }
+
+    private setValue(inputValue:string){
+        let mode = this.mode.find(mode=>mode.reg.test(inputValue));
+        if(!mode) return;
+
+        let btmInputValue = this.getValue(inputValue,mode);
+        this.setState({
+            topInputValue:inputValue,
+            topInputLabelName:mode.topName,
+            btmInputValue:btmInputValue,
+            btmInputLabelName:mode.btmName
+        },()=>{
+            let input = this.btmInputElement.current as HTMLInputElement;
+            input.focus();
+            input.select()
+            console.log(input.value);
+            document.execCommand("copy");
+        });
+
+       
     }
     
-    inputElement  = React.createRef();
-
-    componentDidMount(){
-        console.log(ColorMode);
-        let input = (this.inputElement.current as HTMLInputElement);
-        input.focus()
-        document.execCommand("paste");
-        let inputValue = input.value.trim();
-        console.log(inputValue);
-    }
-
-    getMode(str:string){
-        str[str.length-1] == "," && (str =  str.substring(0, str.length - 1))
-        str[0] == "#" && (str =  str.substring(1, str.length));
-        let pattern = new RegExp("[, ]");
-        if(str.includes(",") || str.includes(" ")){
-            switch (str.split(str.includes(",") ? ",":" ").length) {
-                case 3:
-                    return ColorMode.RGB
-                case 4:
-                    return ColorMode.RGBA
-                default:
-                    return ColorMode.UKN
-            }
-        }
-        else{
-            switch (str.length) {
-                case 6:
-                    return ColorMode.HEX
-                case 8:
-                    return ColorMode.HEXA
-                default:
-                    return ColorMode.UKN
-            }
+    private getValue(inptutValue:string,mode:Mode){
+        let [,r,g,b,a] = mode.reg.exec(inptutValue)
+        switch (mode.mode) {
+            case ColorMode.RGB:
+                return this.RGBtoHEX([r,g,b]);
+            case ColorMode.RGBA:
+                return this.RGBtoHEX([r,g,b],a);
+            case ColorMode.HEX:
+                return this.HEXtoRGB([r,g,b]);
+            case ColorMode.HEXA:
+                return this.HEXtoRGB([r,g,b],a);
+            case ColorMode.HEXS:
+                return "";
+            default:
+                return ""
         }
     }
 
+    private RGBtoHEX(rgb:string[],alpha:string = ""):string{
+        let color = rgb.map((item)=>{
+            return parseInt(item).toString(16).toUpperCase();
+        })
+        alpha && color.push(Math.round( (parseFloat(alpha)*255)).toString(16).toUpperCase() )
+        return "#"+color.join("")
+    }
 
-    render() {
-        return <div className="page" >
+    private HEXtoRGB(rgb:string[],alpha:string = ""):string{
+        let color = rgb.map((item)=>{
+            return parseInt(item,16).toString()
+        });
+        alpha && color.push(( parseInt(alpha,16)/255).toFixed(2) );
+        return alpha ? `rgba(${color.join(",")})` : `rgb(${color.join(",")})`;
+    }
+    
 
+    public render() {
+        return <Container style={{padding:12,textAlign:"center"}} maxWidth={false} >
+
+            <input style={{display:"none"}} ref={this.inputElement} />
             <TextField
-                inputRef={this.inputElement}
-                autoFocus={true}
-                margin="dense"
                 label={this.state.topInputLabelName}
+                value={this.state.topInputValue}
+                inputRef={this.topInputElement}
+                margin="dense"
                 variant="outlined"
                 className="text-field"
                 fullWidth={true}
@@ -71,13 +147,18 @@ export default class HomePage extends React.Component {
                         root:"root",
                         notchedOutline:"notchedOutline",
                         focused: "focused",
+                        input:"input"
                     },
                 }}
             />
 
             <TextField
+                style={{marginTop:"1em"}}
                 className="text-field"
-                label={this.state.bottomInputLabelName}
+                autoFocus={true}
+                label={this.state.btmInputLabelName}
+                value={this.state.btmInputValue}
+                inputRef={this.btmInputElement}
                 margin="dense"
                 variant="outlined"
                 fullWidth={true}
@@ -91,9 +172,21 @@ export default class HomePage extends React.Component {
                         root:"root",
                         notchedOutline:"notchedOutline",
                         focused: "focused",
+                        input:"input"
                     },
                 }}
             />
-        </div>
+            <Fab
+                style={{marginTop:"1em"}}
+                variant="extended"
+                size="medium"
+                color="secondary"
+                aria-label="Add"
+                className="fad-button"
+                classes={{root:"root",label:"label"}}
+            >
+                <FileCopyIcon fontSize="small" style={{marginRight:"5px"}}/>点击复制
+            </Fab>
+        </Container>
     }
 }
